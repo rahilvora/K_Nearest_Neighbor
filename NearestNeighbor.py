afaf8cc3b4af35d4644101946d4b0138e285590f
rahil vora <rahilvora@rahils-MacBook-Pro.local>
2016-11-12 19:39:26 -0800

    completes cosine similarity module

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -15,18 +15,16 @@ def readFile(file):
 
 sumOfSquareRoots = []
 def getTermFrequencyDict(termAndFrequencies):
-    sumOfSquares = 0.0
     termFrequenciesDict = {}
     noOfTerms = len(termAndFrequencies) / 2
     for i in range(0, len(termAndFrequencies), 2):
         normalizedTF = float(termAndFrequencies[i + 1]) / noOfTerms
         termFrequenciesDict[termAndFrequencies[i]] = normalizedTF
-        sumOfSquares += normalizedTF ** 2
         if (documentFrequency.has_key(termAndFrequencies[i])):
             documentFrequency[termAndFrequencies[i]] += 1
         else:
             documentFrequency[termAndFrequencies[i]] = 1
-    sumOfSquareRoots.append(math.sqrt(sumOfSquares))
+
     allTermFrequencies.append(termFrequenciesDict)
 
 def getIDFForAllTerms():
@@ -36,23 +34,17 @@ def getIDFForAllTerms():
 def calculateCosineSimilarityForAPair(doc1, doc2, row, col):
     global similarDocsCount
     dot_product = 0
-    sum1 = 0
-    sum2 = 0
     for key in doc1:
         if key in doc2:
             dot_product += float(doc1[key]) * float(doc2[key])
-    # for item in doc1:
-    #     sum1 += float(doc1[item]) ** 2
-    #
-    # for item in doc2:
-    #     sum2 += float(doc2[item]) ** 2
     cosineSimilarity = dot_product / (sumOfSquareRoots[row] * sumOfSquareRoots[col])
     if cosineSimilarity < epsilon: return
     try:
         cosineSimilaritiesDict[row + 1].append((col+1, cosineSimilarity))
     except:
-        cosineSimilaritiesDict[row + 1] = []
-        cosineSimilaritiesDict[row + 1].append((col+1, cosineSimilarity))
+        currentCosineSimilarity = []
+        currentCosineSimilarity.append((col+1, cosineSimilarity))
+        cosineSimilaritiesDict[row + 1] = currentCosineSimilarity
     similarDocsCount += 1
 
 def calculateCosineSimilarities():
@@ -73,17 +65,20 @@ def calculateCosineSimilarities():
             else:
                 calculateCosineSimilarityForAPair(allTermFrequencies[i], allTermFrequencies[j], i, j)
         if((i + 1) in cosineSimilaritiesDict.keys()):
-            cosineSimilaritiesDict[i + 1] = sortCosineSimilarities(cosineSimilaritiesDict[i + 1])
-            if(len(cosineSimilaritiesDict[i + 1]) > k):
-                cosineSimilaritiesDict[i + 1] = cosineSimilaritiesDict[i + 1][:k]
+            cosineSimilarityTemp = sortCosineSimilarities(cosineSimilaritiesDict[i + 1])
+            if(len(cosineSimilarityTemp) > k):
+                cosineSimilaritiesDict[i + 1] = cosineSimilarityTemp[:k]
 
+def getTFIDF():
+    for i in range(0, len(allTermFrequencies)):
+        sumOfSquares = 0
+        for term in allTermFrequencies[i].keys():
+            allTermFrequencies[i][term] = allTermFrequencies[i][term]*IDF[term]
+            sumOfSquares += allTermFrequencies[i][term] ** 2
+        sumOfSquareRoots.append(math.sqrt(sumOfSquares))
 
 def sortCosineSimilarities(cosineSimilaritiest):
     return sorted(cosineSimilaritiest, key=lambda x: x[1], reverse = True)
-
-# def calculateTFIDF():
-#     for i in range(0, len(allTermFrequencies)):
-#
 
 def saveToOutputFile(outputFile):
     outputStr = ""
@@ -111,7 +106,11 @@ if __name__ == "__main__":
     epsilon = args['eps']
     k = args['k']
     readFile(wiki1File)
+    getIDFForAllTerms()
+    getTFIDF()
+    startTime2 = time.time()
     calculateCosineSimilarities()
+    print("--- %s seconds Cosine Similarity ---" % (time.time() - startTime2))
     saveToOutputFile(outputFile)
     print "Number of neighbors " + str(similarDocsCount*2)
     print("--- %s seconds ---" % (time.time() - startTime))
